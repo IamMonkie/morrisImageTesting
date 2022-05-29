26ffa2eec896d56b447740a8d7fc8390820b8017
twilliams-Morris <twilliams@morris-associates.com>
2022-05-29 00:15:03 -0400

    drag and drop functionality started

---
+++ assets/buttonFunctionality.js
@@ -0,0 +1,20 @@
+/* ----------------------------- runs at startup ---------------------------- */
+document.addEventListener("DOMContentLoaded", (e) => {
+  console.log("Document is Ready");
+
+  /* -------------------------------- Variables ------------------------------- */
+  let i = 0;
+  let numOfImageSlots = 1;
+
+  //   let sort = document.getElementById("buttonContainer", "iconContainer");
+  /* --------------------------------- Scripts -------------------------------- */
+
+  /* -------------------------------- Functions ------------------------------- */
+
+  // imageSlots
+  function addImageSlots() {
+    for (; i < length; i++) {}
+  }
+
+  //Sort
+});
